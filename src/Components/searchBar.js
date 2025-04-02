8152e97102cc30edf61e6f3e315c7593fb5b4ac7
SeelyksDev <kevincordier.dev@gmail.com>
2025-04-02 14:41:27 +0200

    feat: add refresh handling and no recipes found message

--- src/Components/searchBar.js
+++ src/Components/searchBar.js
@@ -1,27 +1,46 @@
 import { displayRecipes, getRecipes } from "../script";
+const recipesWrapper = document.querySelector(".recipes-wrapper");
 
-export function handleSearchBar() {
+async function loadRecipes() {
+    let allRecipes = await getRecipes();
+    return allRecipes;
+}
+
+export async function handleSearchBar() {
     const crossBtn = document.querySelector(".cross-btn");
     const searchField = document.querySelector(".search-field");
+    const searchBtn = document.querySelector(".search-btn");
+    const recipes = await loadRecipes();
 
-    searchField.addEventListener("input", async () => {
+    searchField.addEventListener("input", () => {
         crossBtn.classList.toggle(
             "cross-visible",
             searchField.value.trim().length >= 1
         );
-        const recipes = await getRecipes();
 
-        filterByLetter(searchField, recipes);
+
+        if (searchField.value.trim().length === 0) {
+            recipesWrapper.innerHTML = "";
+            displayRecipes(recipes);
+        }
+
+        if (isValid(searchField)) {
+            filterByLetter(searchField, recipes);
+        }
     });
 
     crossBtn.addEventListener("click", (e) => {
         e.preventDefault();
         searchField.value = "";
         crossBtn.classList.remove("cross-visible");
+        recipesWrapper.innerHTML = "";
+        displayRecipes(recipes);
     });
+
+    searchBtn.addEventListener("click", (e) => e.preventDefault());
 }
 
-export function isValid(search) {
+ function isValid(search) {
     if (search.value.trim().length >= 3) {
         return true;
     } else {
@@ -30,36 +49,42 @@ export function isValid(search) {
 }
 
 export function filterByLetter(search, recipes) {
-    const recipesWrapper = document.querySelector(".recipes-wrapper");
     let filteredRecipes = [];
 
-    if (isValid(search)) {
-        for (let i = 0; i < recipes.length; i++) {
-            let recipeMatches =
-                recipes[i].name.includes(search.value) ||
-                recipes[i].description.includes(search.value);
+    for (let i = 0; i < recipes.length; i++) {
+        let recipeMatches =
+            recipes[i].name.includes(search.value) ||
+            recipes[i].description.includes(search.value);
 
-            for (let j = 0; j < recipes[i].ingredients.length; j++) {
-                if (
-                    recipes[i].ingredients[j].ingredient.includes(search.value)
-                ) {
-                    recipeMatches = true;
-                    break;
-                }
-            }
-
-            if (recipeMatches) {
-                filteredRecipes.push(recipes[i]);
+        for (let j = 0; j < recipes[i].ingredients.length; j++) {
+            if (recipes[i].ingredients[j].ingredient.includes(search.value)) {
+                recipeMatches = true;
+                break;
             }
         }
 
-        recipesWrapper.innerHTML = "";
-
-        for (let i = 0; i < filteredRecipes.length; i++) {
-            displayRecipes(filteredRecipes[i]);
+        if (recipeMatches) {
+            filteredRecipes.push(recipes[i]);
         }
+    }
+
+    if (filteredRecipes.length === 0) {
+        displayNoContentMsg(search);
     } else {
         recipesWrapper.innerHTML = "";
-        displayRecipes(recipes);
+        displayRecipes(filteredRecipes);
     }
+}
+
+function displayNoContentMsg(search) {
+    recipesWrapper.innerHTML = "";
+
+    const p = document.createElement("p");
+    p.classList.add("no-content-msg");
+    p.textContent = `Aucune recette ne contient
+        « ${search.value} »  vous pouvez chercher « tarte aux pommes », « poisson », etc.`;
+    recipesWrapper.appendChild(p);
+
+    const numberRecipes = document.querySelector(".number-recipes");
+    numberRecipes.textContent = "0 recettes";
 }
