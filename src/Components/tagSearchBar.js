d36eaaa598ed3d451c772741ec65ca020dfe123f
SeelyksDev <kevincordier.dev@gmail.com>
2025-04-24 20:42:34 +0200

    Search tag system

--- src/Components/tagSearchBar.js
+++ src/Components/tagSearchBar.js
@@ -1,11 +1,10 @@
 import { isValid, getTags } from "./searchBar";
+import { Tag } from "../Models/Tag";
 
 export function handleTagSearchBar(recipes) {
     const filtersWrapper = document.querySelectorAll(
         ".filters-wrapper .filter"
     );
-
-    console.log(recipes[0]);
 
     filtersWrapper.forEach((filter) => {
         const tagCrossBtn = filter.querySelector(".tag-cross-btn");
@@ -43,20 +42,66 @@ export function handleTagSearchBar(recipes) {
 }
 
 function tagFilterByLetter(search, recipes, category) {
-    let filteredRecipes = [];
+    let filteredTags = new Set([]);
     const searchValue = search.value.toLowerCase();
-    for (let i = 0; i < recipes.length; i++) {
-        switch (category) {
-            case "ingredients":
-                let recipeMatches =
-                recipes[i].name.toLowerCase().includes(searchValue);
-                break;
-            case "appliances":
-                //include sur appliance
-                break;
-            case "ustensils":
-                //include sur ustensils
-                break;
-        }
+
+    switch (category) {
+        case "ingredients":
+            filteredTags = new Set([]);
+            recipes.forEach((el) => {
+                el.ingredients.forEach((el) => {
+                    if (el.ingredient.toLowerCase().includes(searchValue)) {
+                        filteredTags.add(el.ingredient);
+                    }
+                });
+            });
+            createTags(filteredTags, "ingredients");
+            break;
+        case "appliances":
+            filteredTags = new Set();
+            recipes.forEach((el) => {
+                if (el.appliance.toLowerCase().includes(searchValue)) {
+                    filteredTags.add(el.appliance);
+                }
+            });
+            createTags(filteredTags, "appliances");
+            break;
+        case "ustensils":
+            filteredTags = new Set([]);
+            recipes.forEach((el) => {
+                el.ustensils.forEach((ustensil) => {
+                    if (ustensil.toLowerCase().includes(searchValue)) {
+                        filteredTags.add(ustensil);
+                    }
+                });
+            });
+            createTags(filteredTags, "ustensils");
+            break;
     }
+}
+
+function createTags(filteredTags, category) {
+    const ingredientsContainer = document.querySelector(".ingredients-list");
+    const ustensilsContainer = document.querySelector(".ustensils-list");
+    const appliancesContainer = document.querySelector(".appliances-list");
+    let currentCategory = "";
+
+    switch (category) {
+        case "ingredients":
+            currentCategory = ingredientsContainer;
+            break;
+        case "appliances":
+            currentCategory = appliancesContainer;
+            break;
+        case "ustensils":
+            currentCategory = ustensilsContainer;
+            break;
+    }
+
+    currentCategory.innerHTML = "";
+
+    filteredTags.forEach((el) => {
+        let tagTemplate = new Tag(el);
+        currentCategory.appendChild(tagTemplate.displayTag());
+    });
 }
